3d8838f88bb76b36700584df17d9f849aad00121
mza18 <maximilian.zarna@tu-clausthal.de>
2026-09-23 15:41:37 +0200

    feat: Add api-handler for /reset and /metrics

--- main.go
+++ main.go
@@ -1,6 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) fileServerHitsHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	body := fmt.Sprintf("Hits: %v", cfg.fileServerHits.Load())
+	w.Write([]byte(body))
+}
+
+func (cfg *apiConfig) resetFileServerHits(w http.ResponseWriter, req *http.Request) {
+	cfg.fileServerHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("FileServerHits have been set back to 0"))
+}
 
 func healtzHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -9,11 +38,14 @@ func healtzHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	mux := http.NewServeMux()
+	apiCfg := apiConfig{atomic.Int32{}}
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.Handle("/assets/", http.FileServer(http.Dir("assets/")))
-	mux.HandleFunc("/healthz", healtzHandler)
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("GET /api/healthz", healtzHandler)
+	mux.HandleFunc("GET /api/metrics", apiCfg.fileServerHitsHandler)
+	mux.HandleFunc("POST /api/reset", apiCfg.resetFileServerHits)
 
 	server := &http.Server{
 		Handler: mux,
